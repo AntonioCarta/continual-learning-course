4fdf566cc78b814f5927bfa5fbb0c192e5bbe062
Antonio Carta <antonio.carta@di.unipi.it>
2023-04-13 13:03:25 +0200

    ADD KTA5 - prototype networks

---
+++ utils/__init__.py
@@ -0,0 +1,34 @@
+from utils.omniglot_dataset import OmniglotDataset
+
+import numpy as np
+import torch
+
+
+def load_omniglot_data(opt, mode):
+    dataset = OmniglotDataset(mode=mode, root=opt.dataset_root)
+    n_classes = len(np.unique(dataset.y))
+    if n_classes < opt.classes_per_it_tr or n_classes < opt.classes_per_it_val:
+        raise(Exception('There are not enough classes in the dataset in order ' +
+                        'to satisfy the chosen classes_per_it. Decrease the ' +
+                        'classes_per_it_{tr/val} option and try again.'))
+    return dataset
+
+
+def init_seed(opt):
+    '''
+    Disable cudnn to maximize reproducibility
+    '''
+    torch.cuda.cudnn_enabled = False
+    np.random.seed(opt.manual_seed)
+    torch.manual_seed(opt.manual_seed)
+    torch.cuda.manual_seed(opt.manual_seed)
+
+
+def init_seed(opt):
+    '''
+    Disable cudnn to maximize reproducibility
+    '''
+    torch.cuda.cudnn_enabled = False
+    np.random.seed(opt.manual_seed)
+    torch.manual_seed(opt.manual_seed)
+    torch.cuda.manual_seed(opt.manual_seed)
